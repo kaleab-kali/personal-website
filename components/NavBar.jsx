8e302669140f3d1b359488dc0c92efd68ea0908a
kaliktg <86680782+kaliktg@users.noreply.github.com>
2023-04-03 12:38:48 +0000

    added a nav bar

---
+++ components/NavBar.jsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src="/../public/assets/logo.png"
+          alt="/"
+          width="125"
+          height="50"
+        />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="../public/assets/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="../public/assets/">
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="../public/assets/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Skill</li>
+            </Link>
+            <Link href="../public/assets/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Project
+              </li>
+            </Link>
+            <Link href="../public/assets/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+          <div>
+        
+          </div>
+        </div>{/*end of buttons*/}
+       
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
